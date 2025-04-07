bc9c6e6e7193f7974231ebc08f7003ea01a56cea
nink2458 <nink2458@gmail.com>
2025-04-07 21:21:27 +0900

    commit

--- src/main/java/com/nhnacademy/eureka/EurekaApplication.java
+++ src/main/java/com/nhnacademy/eureka/EurekaApplication.java
@@ -2,12 +2,14 @@ package com.nhnacademy.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudEurekaApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
